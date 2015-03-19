ee443be358fde2fa89a476e712fcfe1101182ca8
DJBuro <DJBuro@gmail.com>
2015-03-19 12:52:28 +0200

    Added Random Generator!

---
+++ Game/SuperAdventure/Engine/RandomNumberGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace Engine
+{
+    using System;
+    using System.Security.Cryptography;
+    public static class RandomNumberGenerator
+    {
+        private static readonly RNGCryptoServiceProvider generator = new RNGCryptoServiceProvider();
+
+        public static int NumberGenerate(int minValue, int maxValue)
+        {
+            byte[] data = new byte[4];
+
+            generator.GetBytes(data);
+
+            int value = BitConverter.ToInt32(data, 0);
+            
+            return value;
+        }
+    }
+}
